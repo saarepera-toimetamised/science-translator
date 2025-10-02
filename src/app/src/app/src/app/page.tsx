9282355ec04b9c35283962053faddfc94e09a9fd
saarepera-toimetamised <saarepera@gmail.com>
2025-10-02 14:13:27 +0300

    Delete src directory

--- src/app/src/app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import TranslatorUI from './components/TranslatorUI';  
-  
-export default function Home() {  
-  return (  
-    <main className="min-h-screen bg-gray-50">  
-      <TranslatorUI />  
-    </main>  
-  );  
-}  
