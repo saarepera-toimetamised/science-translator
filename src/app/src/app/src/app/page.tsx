9737ef195255825eb0291889f1bbe173a10bc002
saarepera-toimetamised <saarepera@gmail.com>
2025-10-02 14:06:10 +0300

    Create page.tsx

---
+++ src/app/src/app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import TranslatorUI from './components/TranslatorUI';  
+  
+export default function Home() {  
+  return (  
+    <main className="min-h-screen bg-gray-50">  
+      <TranslatorUI />  
+    </main>  
+  );  
+}  
